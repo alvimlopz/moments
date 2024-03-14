1261b03dc548599bd0b5657643a2b14bef9b4ce9
alvaro rodrigues <alvim.lopez@gmail.com>
2024-03-14 18:42:46 -0300

    Iniciando o componente de mensagem

--- src/app/components/messages/messages.component.ts
+++ src/app/components/messages/messages.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+
+import { MessageService } from '../../services/message.service';
 
 @Component({
   selector: 'app-messages',
   templateUrl: './messages.component.html',
   styleUrl: './messages.component.css'
 })
-export class MessagesComponent {
+export class MessagesComponent implements OnInit{
+
+  faTimes = faTimes;
+
+  constructor(public messageService: MessageService){
+
+  }
+  ngOnInit(): void {
+  }
 
 }
