65374f2f008f6d98540b3d1e4f3316cfa12f39af
alvaro rodrigues <alvim.lopez@gmail.com>
2024-03-15 10:29:46 -0300

    Preparando o sistema para receber dados do banco

--- src/app/componenst/pages/home/home.component.ts
+++ src/app/componenst/pages/home/home.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { MomentService } from '../../../services/moment.service';
+import { Moment } from '../../../moments';
+import { environment } from '../../../../environment/environment';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+  allMoments: Moment[] = []
+  moment: Moment[] = []
+  baseApiUrl = environment.baseApiUrl
+
+  constructor(){}
+
+  ngOnInit(): void {
+  }
 
 }
