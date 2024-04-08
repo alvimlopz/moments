4a0b4d97d88569f25400dcedf166013013835fac
alvaro rodrigues <alvim.lopez@gmail.com>
2024-04-08 13:44:39 -0300

    Criando o componente de rotas

---
+++ src/app/components/pages/edit-moment/edit-moment.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { ActivatedRoute, Route } from '@angular/router';
+
+import { Moment } from '../../../moments';
+import { MomentService } from '../../../services/moment.service';
+
+@Component({
+  selector: 'app-edit-moment',
+  templateUrl: './edit-moment.component.html',
+  styleUrl: './edit-moment.component.css'
+})
+export class EditMomentComponent {
+
+}
